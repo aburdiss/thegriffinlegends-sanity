42b5387be2a4fa9ed0449bf3f786cd393969baf9
Alexander Burdiss <aburdiss@icloud.com>
2025-04-08 20:02:46 -0400

    feat: add studiohost

--- sanity.cli.ts
+++ sanity.cli.ts
@@ -1,13 +1,14 @@
-import {defineCliConfig} from 'sanity/cli'
+import { defineCliConfig } from 'sanity/cli';
 
 export default defineCliConfig({
   api: {
     projectId: 'qqm8v4gw',
-    dataset: 'production'
+    dataset: 'production',
   },
+  studioHost: 'thegriffinlegends',
   /**
    * Enable auto-updates for studios.
    * Learn more at https://www.sanity.io/docs/cli#auto-updates
    */
   autoUpdates: true,
-})
+});
