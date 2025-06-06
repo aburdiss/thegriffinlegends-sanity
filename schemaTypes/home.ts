2c9ccdb66ff9c5bd4efd065e4553c4d38049dc4a
Alexander Burdiss <aburdiss@icloud.com>
2025-06-06 19:45:00 -0400

    feat: update home page to be fully dynamic

--- schemaTypes/home.ts
+++ schemaTypes/home.ts
@@ -1,4 +1,4 @@
-import {defineField, defineType} from 'sanity'
+import { defineField, defineType } from 'sanity';
 
 export default defineType({
   name: 'home',
@@ -9,29 +9,59 @@ export default defineType({
       name: 'firstSectionHeadline',
       title: 'First Section Headline',
       type: 'string',
+      validation: (Rule) => [Rule.required()],
     }),
     defineField({
       name: 'firstSectionText',
       title: 'First Section Text',
       type: 'array',
-      of: [{type: 'block'}],
+      of: [{ type: 'block' }],
     }),
     defineField({
       name: 'firstSectionImage',
       title: 'First Section Image',
       type: 'image',
+      fields: [
+        {
+          name: 'alt',
+          type: 'string',
+          title: 'Alternative text',
+          description: 'Alternative text is required.',
+          hidden: ({ parent }) => !parent?.asset,
+          validation: (Rule) => [Rule.required()],
+        },
+      ],
     }),
-    // ADD FEATURED BOOK HERE
+    defineField({
+      name: 'featuredBookHeadline',
+      title: 'Featured Book Headline',
+      type: 'string',
+      validation: (Rule) => [Rule.required(), Rule.max(40)],
+    }),
+    defineField({
+      name: 'featuredBook',
+      title: 'Featured Book',
+      type: 'reference',
+      to: [{ type: 'book' }],
+      validation: (Rule) => [Rule.required()],
+    }),
+    defineField({
+      name: 'featuredBookCta',
+      title: 'Fetaured Book CTA',
+      type: 'link',
+      validation: (Rule) => [Rule.required()],
+    }),
     defineField({
       name: 'contactSectionHeadline',
       title: 'Contact Section Headline',
       type: 'string',
+      validation: (Rule) => [Rule.required()],
     }),
     defineField({
       name: 'contactSectionText',
       title: 'Contact Section Text',
       type: 'array',
-      of: [{type: 'block'}],
+      of: [{ type: 'block' }],
     }),
   ],
-})
+});
